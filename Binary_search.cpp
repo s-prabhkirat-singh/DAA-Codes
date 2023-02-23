1009971ce20a9df1a2820fa371cff9b002813fa2
Prabhkirat Singh <prabhkiratsingh2278@gmail.com>
2023-02-23 22:11:07 +0530

    first commit

---
+++ Binary_search.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Creating a 400 size array";
+    int arr[4];
+    arr[0] = 10;
+    int i = 0;
+
+    while (true)
+    {
+
+        int x = rand() % 100;
+        cout << "X is : " << x << endl;
+        cout << i;
+        if (x > arr[i])
+        {
+            arr[i + 1] = x;
+            i++;
+        }
+
+        if (i == 10)
+        {
+            break;
+        }
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        cout << arr[i] << "," << endl;
+    }
+}
